ad53df0cd000a52047a71c0aeff6e3cf3ebbc115
juvtorg2014 <117074157+juvtorg2014@users.noreply.github.com>
2023-10-13 20:42:12 +0300

    Добавлена новая колонка "Сумма" и файл сделок

--- lchi.py
+++ lchi.py
@@ -3,14 +3,33 @@ import numpy as np
 import os
 
 
+def save_file(source, main, new, name):
+    with open(name_file) as fw:
+        common_file = main + new + '\\' + name + '.csv'
+        source.to_csv(common_file, header=True, index=False, encoding='utf-8', sep=';')
+        
+
 def load_file(file_name):
     main_dir = os.path.dirname(file_name) + '\\'
     new_dir = file_name.split('\\')[-1][:-4]
     new_name = file_name.split('\\')[-1][2:]
     if not os.path.exists(main_dir + new_dir):
         os.mkdir(main_dir + new_dir)
-    source = pd.read_csv(file_name, sep=" |;")
-    source.columns = ['Date', 'Time', 'Tiker', 'Quant', 'Price']
+    source = pd.read_csv(file_name, sep=";", header=None)
+    source.columns = ['Time', 'Tiker', 'Quant', 'Price']
+    date_time = source['Time'].str.split(' ', expand=True)
+    date_time.columns = ['Date', 'Time']
+    
+    source.insert(0, 'Date', True)
+    source['Date'] = date_time['Date']
+    source['Time'] = date_time['Time']
+    source['Tiker'] = source['Tiker'].str.strip()
+    
+    time = source['Time'].str.split('.', expand=True)
+    time.columns = ['Time', 'Time2']
+    source['Time'] = time['Time']
+    save_file(source, main_dir, new_dir, name)
+    
     tickers = sorted(source['Tiker'].unique().tolist())
     source = source.groupby(['Date', 'Time', 'Tiker', 'Price'], as_index=False)['Quant'].sum()
     source = source.sort_values(by=['Tiker', 'Date', 'Time'])
@@ -26,7 +45,9 @@ def load_file(file_name):
                 new_summ.append(source['Quant'].values[n])
         
     # Добавление новой колонки и сохранение результата в файл
+    #source = source.sort_values(by=['Date', 'Time'])
     source['Summ'] = new_summ
+    source['Money'] =  source['Quant'] * source['Price']
     common_file = main_dir + new_dir + '\\' + new_name
     source.to_csv(common_file, header=True, index=False, encoding='utf-8', sep=';')
 
@@ -35,10 +56,11 @@ def load_file(file_name):
         df = source[np.logical_not(source['Tiker'] != item)]
         newfile = main_dir + new_dir + '\\' + item + '.csv'
         df.to_csv(newfile, header=True, index=False, encoding='utf-8', sep=';')
-        print(df.tail(2))
+     
         
 
 if __name__ == '__main__':
-    name = input("Введите имя фала без расширения:\n")
+    #name = input("Введите имя фала без расширения:\n")
+    name = '1_282722'
     name_file = os.path.abspath(name) + '.csv'
     load_file(name_file)
